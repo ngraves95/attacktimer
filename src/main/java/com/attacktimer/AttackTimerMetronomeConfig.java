ngraves95/attacktimer

commit 7fa62c50cabc700a4c20d17512b982252d428ed9

Yama (#140)
## Summary

Supersedes https://github.com/ngraves95/attacktimer/pull/130, this has all the same functionality but also handles the shadow crashing :) this makes Yama fully functional with the plugin and it shouldn't mis-count anymore!

This is the first feature which is working outside the `onGameTick` as it's graphics object based.

### Yama Shadow Crash

This is implemented unfortunately in `onRender` since we use the literal graphics of the fireballs to determine, if the player is in the correct tile and the timing is right. Timing was easy as we can just look for animation frame to be the correct tick. The location was a bit trickier since this was the first time I've really thought about that part of the client API.

Because this is being called in `onRender` we make sure to return early if the player is not in the correct region and yama isn't actually in P3. However once those conditions are true there is some wasteful computation as every cycle we re-compute all the fireball lines, however I don't think this takes long enough to actually affect performance negatively.

### Purging Staff

Original description for Purging staff spec:

Some reasoning about why this is hard coded to Yama, **there's 3 pieces to the purging staff puzzle**:

1. Are we specing on this attack?
2. How much damage are we hitting?
3. How much hp does our target have?

If we can answer all three questions we can solve the spec. This PR currently answers (1.) and (2.) albeit quite poorly and in a brittle way [1], however the 3rd question is actually the hardest 😅 because the client isn't given an NPC's health directly only a funny ratio number:
```java
	/**
	 * Gets the health of the actor in {@link #getHealthScale()} units.
	 *
	 * The server does not transmit actors' real health, only this value
	 * between zero and {@link #getHealthScale()}. Some actors may be
	 * missing this info, in which case -1 is returned.
	 */
	int getHealthRatio();

	/**

runelite-plugin.properties
@@ -1,6 +1,7 @@
 displayName=AttackTimer
 author=ngraves95,Lexer747
 build=standard
+version=1.2.0
 description=A plugin to countdown until your next attack
 tags=pvm,timer,attack,combat,weapon
 plugins=com.attacktimer.AttackTimerMetronomePlugin

src/main/java/com/attacktimer/AttackTimerMetronomePlugin.java
@@ -28,7 +28,9 @@ package com.attacktimer;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import com.attacktimer.ClientUtils.Utils;
 import com.attacktimer.VariableSpeed.VariableSpeed;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.io.ByteArrayDataOutput;
@@ -36,6 +38,7 @@ import com.google.inject.Provides;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -44,19 +47,19 @@ import java.util.regex.Pattern;
 import javax.inject.Inject;
 import net.runelite.api.Actor;
 import net.runelite.api.Client;
-import net.runelite.api.EquipmentInventorySlot;
-import net.runelite.api.InventoryID;
-import net.runelite.api.Item;
-import net.runelite.api.ItemContainer;
 import net.runelite.api.NPC;
 import net.runelite.api.Player;
+import net.runelite.api.Skill;
 import net.runelite.api.Varbits;
 import net.runelite.api.events.ChatMessage;
+import net.runelite.api.events.FakeXpDrop;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.InteractingChanged;
 import net.runelite.api.events.SoundEffectPlayed;
+import net.runelite.api.events.StatChanged;
 import net.runelite.api.events.VarClientIntChanged;
 import net.runelite.api.events.VarbitChanged;
+import net.runelite.api.gameval.VarPlayerID;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
@@ -123,9 +126,18 @@ public class AttackTimerMetronomePlugin extends Plugin
     private int lastEquippingMonotonicValue = -1;
     private int soundEffectTick = -1;
     private int soundEffectId = -1;
+    private boolean isUsingMagic = false;
 
     public int pendingEatDelayTicks = 0;
 
+    private ArrayDeque<Integer> specialPercentageEvents = new ArrayDeque<Integer>();
+    private Map<Skill, ArrayDeque<Integer>> combatExpEarned = Map.of(
+        Skill.MAGIC, new ArrayDeque<Integer>(),
+        Skill.RANGED, new ArrayDeque<Integer>(),
+        Skill.DEFENCE, new ArrayDeque<Integer>(),
+        Skill.STRENGTH, new ArrayDeque<Integer>(),
+        Skill.ATTACK, new ArrayDeque<Integer>()
+    );
 
     private static final int UI_HIDE_DEBOUNCE_TICKS_MAX = 1;
     private static final int ATTACK_DELAY_NONE = 0;
@@ -188,6 +200,10 @@ public class AttackTimerMetronomePlugin extends Plugin
         {
             currentSpellBook = Spellbook.fromVarbit(varbitChanged.getValue());
         }
+        if (varbitChanged.getVarpId() == VarPlayerID.SA_ENERGY)
+        {
+            specialPercentageEvents.addLast(varbitChanged.getValue());
+        }
     }
 
     // onVarbitChanged happens when the user causes some interaction therefore we can't rely on some fixed
@@ -227,6 +243,36 @@ public class AttackTimerMetronomePlugin extends Plugin
         soundEffectId = event.getSoundId();
     }
 
+    @Subscribe
+    protected void onFakeXpDrop(FakeXpDrop event)
+    {
+        if (!combatExpEarned.containsKey(event.getSkill()))
+        {
+            return;
+        }
+        combatExpEarned.get(event.getSkill()).addLast(event.getXp());
+        if (attackState == AttackState.DELAYED_FIRST_TICK)
+        {
+            // We recompute attack speed here incase the hitsplat mattered (e.g. purging staff)
+            performAttack();
+        }
+    }
+
+    @Subscribe
+    protected void onStatChanged(StatChanged event)
+    {
+        if (!combatExpEarned.containsKey(event.getSkill()))
+        {
+            return;
+        }
+        combatExpEarned.get(event.getSkill()).addLast(event.getXp());
+        if (attackState == AttackState.DELAYED_FIRST_TICK)
+        {
+            // We recompute attack speed here incase the hitsplat mattered (e.g. purging staff)
+            performAttack();
+        }
+    }
+
     // endregion
 
     @Provides
@@ -235,23 +281,41 @@ public class AttackTimerMetronomePlugin extends Plugin
         return configManager.getConfig(AttackTimerMetronomeConfig.class);
     }
 
-    private int getItemIdFromContainer(ItemContainer container, int slotID)
+    private int computeDamage(AttackStyle attackStyle, AttackProcedure atkType, AnimationData curAnimation)
     {
-        if (container == null)
+        switch (atkType)
         {
-            return -1;
+            case POWERED_STAVE:
+                // TODO not needed for any variable speed
+                return -1;
+            case MANUAL_AUTO_CAST:
+                if (attackStyle == AttackStyle.DEFENSIVE_CASTING || attackStyle == AttackStyle.DEFENSIVE)
+                {
+                    // just use the defense exp to compute the damage
+                    return Utils.getLastDelta(combatExpEarned.get(Skill.DEFENCE));
+                }
+                else
+                {
+                    // deduct the fixed exp based on the spell
+                    // (for now this only works for dark demon bane which awkwardly gives fractional exp)
+                    var mageExp = Utils.getLastDelta(combatExpEarned.get(Skill.MAGIC));
+                    if (curAnimation != AnimationData.MAGIC_ARCEUUS_DEMONBANE)
+                    {
+                        return -1;
+                    }
+                    return (int) Math.ceil(((double) mageExp - 43.5D) / 2.0D);
+                }
+            case MELEE_OR_RANGE:
+                // TODO not needed for any variable speed
+                return -1;
         }
-        final Item item = container.getItem(slotID);
-        return (item != null) ? item.getId() : -1;
+        return -1;
     }
+
 
     private int getWeaponId()
     {
-        int weaponId = getItemIdFromContainer(
-                client.getItemContainer(InventoryID.EQUIPMENT),
-                EquipmentInventorySlot.WEAPON.getSlotIdx()
-        );
-
+        final int weaponId = Utils.getWeaponId(client);
         return WEAPON_ID_MAPPING_WORKAROUNDS.getOrDefault(weaponId, weaponId);
     }
 
@@ -302,27 +366,35 @@ public class AttackTimerMetronomePlugin extends Plugin
 
     private int getWeaponSpeed(int weaponId, PoweredStaves stave, AnimationData curAnimation, boolean matchesSpellbook)
     {
+        var specDelta = Utils.getLastDelta(specialPercentageEvents);
+        int damageDealt = -1;
         if (stave != null && stave.getAnimations().contains(curAnimation))
         {
+            isUsingMagic = true;
+            damageDealt = computeDamage(Utils.getAttackStyle(client), AttackProcedure.POWERED_STAVE, curAnimation);
             // We are currently dealing with a staves in which case we can make decisions based on the
             // spellbook flag. We can only improve this by using a deprecated API to check the projectile
             // matches the stave rather than a manual spell, but this is good enough for now.
-            return VariableSpeed.computeSpeed(client, curAnimation, AttackProcedure.POWERED_STAVE, 4);
+            return VariableSpeed.computeSpeed(client, curAnimation, AttackProcedure.POWERED_STAVE, damageDealt, specDelta, 4);
         }
 
         if (matchesSpellbook && isManualCasting(curAnimation))
         {
+            isUsingMagic = true;
+            damageDealt = computeDamage(Utils.getAttackStyle(client), AttackProcedure.MANUAL_AUTO_CAST, curAnimation);
             // You can cast with anything equipped in which case we shouldn't look to invent for speed.
-            return VariableSpeed.computeSpeed(client, curAnimation, AttackProcedure.MANUAL_AUTO_CAST, getMagicBaseSpeed(weaponId));
+            return VariableSpeed.computeSpeed(client, curAnimation, AttackProcedure.MANUAL_AUTO_CAST, damageDealt, specDelta,getMagicBaseSpeed(weaponId));
         }
 
+        isUsingMagic = false;
+        damageDealt = computeDamage(Utils.getAttackStyle(client), AttackProcedure.MELEE_OR_RANGE, curAnimation);
         ItemStats weaponStats = getWeaponStats(weaponId);
         if (weaponStats == null)
         {
-            return VariableSpeed.computeSpeed(client, curAnimation, AttackProcedure.MELEE_OR_RANGE, 4); // Assume barehanded == 4t
+            return VariableSpeed.computeSpeed(client, curAnimation, AttackProcedure.MELEE_OR_RANGE, damageDealt, specDelta, 4); // Assume barehanded == 4t
         }
         // Deadline for next available attack.
-        return VariableSpeed.computeSpeed(client, curAnimation, AttackProcedure.MELEE_OR_RANGE, weaponStats.getEquipment().getAspeed());
+        return VariableSpeed.computeSpeed(client, curAnimation, AttackProcedure.MELEE_OR_RANGE, damageDealt, specDelta, weaponStats.getEquipment().getAspeed());
     }
 
     private static final List<Integer> SPECIAL_NPCS = Arrays.asList(10507, 9435, 9438, 9441, 9444); // Combat Dummy + Nightmare Pillars
@@ -438,6 +510,7 @@ public class AttackTimerMetronomePlugin extends Plugin
             // We should always add eat delay
             pendingEatDelayTicks += attackDelay;
         }
+        VariableSpeed.onChatMessage(client, event);
     }
 
     // onInteractingChanged is the driver for detecting if the player attacked out side the usual tick window
@@ -456,6 +529,7 @@ public class AttackTimerMetronomePlugin extends Plugin
             switch (attackState)
             {
                 case NOT_ATTACKING:
+                    isUsingMagic = false;
                     // If not previously attacking, this action can result in a queued attack or
                     // an instant attack. If its queued, don't trigger the cooldown yet.
                     if (isPlayerAttacking())
@@ -523,6 +597,17 @@ public class AttackTimerMetronomePlugin extends Plugin
         // clamp the attackDelayHoldoffTicks at -20, this is so we correctly account for eats even when not
         // attacking, but don't count down forever.
         attackDelayHoldoffTicks = Math.max(-20, attackDelayHoldoffTicks - 1);
+        if (specialPercentageEvents.size() > 5)
+        {
+            specialPercentageEvents.removeFirst();
+        }
+        for (var q : combatExpEarned.values())
+        {
+            if (q.size() > 5)
+            {
+                q.removeFirst();
+            }
+        }
     }
 
 
@@ -551,6 +636,7 @@ public class AttackTimerMetronomePlugin extends Plugin
         attackDelayHoldoffTicks = 0;
     }
 
+    @VisibleForTesting
     public void writeState(ByteArrayDataOutput outChannel)
     {
         StringBuilder sb = new StringBuilder();
@@ -570,4 +656,21 @@ public class AttackTimerMetronomePlugin extends Plugin
         outChannel.write(bytes);
     }
     private static final String SEPARATOR = ", ";
+
+
+    public void onRender()
+    {
+        int delta = 0;
+        delta = VariableSpeed.SHADOW_CRASH.onRender(client, attackDelayHoldoffTicks, isUsingMagic);
+
+        if (delta != 0)
+        {
+            attackDelayHoldoffTicks += delta;
+            // if a change in attack delay would cause the delay to be less than 0 we hide the display
+            if (attackDelayHoldoffTicks < 0)
+            {
+                attackState = AttackState.NOT_ATTACKING;
+            }
+        }
+    }
 }

build.gradle
@@ -45,8 +45,11 @@ java {
 	targetCompatibility = "1.11"
 }
 
+def props = new Properties()
+file('runelite-plugin.properties').withInputStream { props.load(it) }
+
 group = 'com.attacktimer'
-version = '1.1'
+version = props.getProperty('version')
 
 tasks.withType(JavaCompile) {
 	options.encoding = 'UTF-8'

src/main/java/com/attacktimer/AnimationData.java
@@ -187,7 +187,7 @@ public enum AnimationData
     MAGIC_ANCIENT_MULTI_TARGET_PVP(1979, AttackStyle.MAGIC, Spellbook.ANCIENT), // Burst & Barrage animations (tested all 8, different weapons)
     MAGIC_ANCIENT_SINGLE_TARGET(10091, AttackStyle.MAGIC, Spellbook.ANCIENT), // Rush & Blitz animations (tested all 8, different weapons)
     MAGIC_ANCIENT_SINGLE_TARGET_PVP(1978, AttackStyle.MAGIC, Spellbook.ANCIENT), // Rush & Blitz animations
-    
+
     MAGIC_ARCEUUS_DEMONBANE(8977, AttackStyle.MAGIC, Spellbook.ARCEUUS), // Also greater corruption, so that may accidentally trigger a manual-cast, but that's probably fine only affects Muspah
     MAGIC_ARCEUUS_GRASP(8972, AttackStyle.MAGIC, Spellbook.ARCEUUS),
 

src/main/java/com/attacktimer/AttackStyle.java
@@ -51,4 +51,10 @@ public enum AttackStyle
         this.name = name;
         this.skills = skills;
     }
+
+    @Override
+    public String toString()
+    {
+        return this.name;
+    }
 }

src/main/java/com/attacktimer/AttackTimerMetronomeTileOverlay.java
@@ -68,6 +68,7 @@ public class AttackTimerMetronomeTileOverlay extends Overlay
     @Override
     public Dimension render(Graphics2D graphics)
     {
+        plugin.onRender();
         player = client.getLocalPlayer();
         plugin.renderedState = plugin.attackState;
         if (plugin.attackState == AttackTimerMetronomePlugin.AttackState.NOT_ATTACKING)

commit daea3c0786022f04bd17add256921d2345c3ab4e

Add logging and fix (#144)
## Summary

Fixes https://github.com/ngraves95/attacktimer/issues/142

Which I can conclude was caused by https://github.com/ngraves95/attacktimer/pull/140, this is because with the extra logging i could see that on exp drop would trigger too late and then essentially and one tick to the timer by accident.

This PR centralises the pre attack window logic.

This also adds logs, this was how I was able to draw such a conclusion.

## Testing

I did a whole Yama kill and the problem didn't occur again.

runelite-plugin.properties
@@ -1,7 +1,7 @@
 displayName=AttackTimer
 author=ngraves95,Lexer747
 build=standard
-version=1.2.0
+version=1.2.1
 description=A plugin to countdown until your next attack
 tags=pvm,timer,attack,combat,weapon
 plugins=com.attacktimer.AttackTimerMetronomePlugin

src/main/java/com/attacktimer/AttackTimerMetronomeConfig.java
@@ -53,12 +53,12 @@ public interface AttackTimerMetronomeConfig extends Config
     @ConfigSection(
             name = "Attack Cooldown Tick Settings",
             description = "Change attack tick cooldown settings",
-            position = 1
+            position = 10
     )
     final String TICK_NUMBER_SETTINGS = "Attack Cooldown Tick Settings";
 
     @ConfigItem(
-            position = 1,
+            position = 10,
             keyName = "showTick",
             name = "Show Attack Cooldown Ticks",
             description = "Shows number of ticks until next attack",
@@ -71,7 +71,7 @@ public interface AttackTimerMetronomeConfig extends Config
 
 
     @ConfigItem(
-            position = 2,
+            position = 20,
             keyName = "disableFontScaling",
             name = "Disable Font Size Scaling (Metronome Tick Only)",
             description = "Disables font size scaling for metronome tick number",
@@ -83,7 +83,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 3,
+            position = 30,
             keyName = "fontSize",
             name = "Font Size (Overhead Tick Only)",
             description = "Change the font size of the overhead attack cooldown ticks",
@@ -96,7 +96,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 4,
+            position = 40,
             keyName = "countColor",
             name = "Tick Number Color",
             description = "Configures the color of tick number",
@@ -108,7 +108,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 5,
+            position = 50,
             keyName = "lastColor",
             name = "Last Tick Color",
             description = "Configures the color of tick number when it says 1",
@@ -120,7 +120,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 6,
+            position = 60,
             keyName = "fontType",
             name = "Font Type",
             description = "Change the font of the tick number",
@@ -132,7 +132,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 7,
+            position = 70,
             keyName = "ticksPosition",
             name = "Ticks Position",
             description = "Position of the tick number respective to the player",
@@ -144,7 +144,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 8,
+            position = 80,
             keyName = "tickHeightOffset",
             name = "Height Offset",
             description = "Height offset for minor adjustments of the tick number",
@@ -157,7 +157,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 9,
+            position = 90,
             keyName = "useZeroBasedTickCount",
             name = "Zero-based Tick Count",
             description = "Count ticks to 0 instead of 1",
@@ -171,12 +171,12 @@ public interface AttackTimerMetronomeConfig extends Config
     @ConfigSection(
             name = "Attack Bar",
             description = "Change the colors and number of colors to cycle through",
-            position = 2
+            position = 20
     )
     final String ATTACK_BAR_SETTINGS = "Attack Cooldown Bar Settings";
 
     @ConfigItem(
-            position = 1,
+            position = 10,
             keyName = "attackBar",
             name = "Show Attack Bar",
             description = "Show the attack bar",
@@ -188,7 +188,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 2,
+            position = 20,
             keyName = "attackBarHeightOffset",
             name = "Height Offset",
             description = "Height offset for the bar from top of player model",
@@ -201,7 +201,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 3,
+            position = 30,
             keyName = "attackBarEmpties",
             name = "Empties Before Attack",
             description = "Controls whether the attack bar will fully empty before a new attack can occur",
@@ -213,7 +213,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 4,
+            position = 40,
             keyName = "attackBarFills",
             name = "Fills Before Attack",
             description = "Controls whether the attack bar will fill completely after an attack",
@@ -225,7 +225,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 5,
+            position = 50,
             keyName = "attackBarDirection",
             name = "Attack Bar Fills or Drains",
             description = "Controls whether the attack bar will fill or drain as a cooldown",
@@ -237,7 +237,7 @@ public interface AttackTimerMetronomeConfig extends Config
     }
 
     @ConfigItem(
-            position = 6,
+            position = 60,
             keyName = "attackBarStyle",
             name = "Attack Bar Style",
             description = "Auto matches HD/SD from Interface Styles plugin. Standard forces the basic bar. High Detail forces the HD bar.",
@@ -246,6 +246,25 @@ public interface AttackTimerMetronomeConfig extends Config
     default AttackBarStyle barStyle()
     {
         return AttackBarStyle.AUTO;
+    }
+
+    @ConfigSection(
+            name = "Attack Timer Debug Settings",
+            description = "Debug",
+            position = 30
+    )
+    final String ATTACK_TIMER_DEBUG_SETTINGS = "Attack Timer Debug Settings";
+
+    @ConfigItem(
+            position = 10,
+            keyName = "debugLogs",
+            name = "Enable Logging",
+            description = "Turns on state machine logging for bugs",
+            section = ATTACK_TIMER_DEBUG_SETTINGS
+    )
+    default boolean debugLogs()
+    {
+        return false;
     }
 
     @Getter

src/main/java/com/attacktimer/AttackTimerMetronomePlugin.java
@@ -45,6 +45,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Actor;
 import net.runelite.api.Client;
 import net.runelite.api.NPC;
@@ -70,6 +71,7 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
 
+@Slf4j
 @PluginDescriptor(
         name = "Attack Timer Metronome",
         description = "Shows a visual cue on an overlay every game tick to help timing based activities",
@@ -225,9 +227,10 @@ public class AttackTimerMetronomePlugin extends Plugin
             return;
         }
         combatExpEarned.get(event.getSkill()).addLast(event.getXp());
-        if (attackState == AttackState.DELAYED_FIRST_TICK)
+        if (inPreAttackWindow())
         {
             // We recompute attack speed here incase the hitsplat mattered (e.g. purging staff)
+            logStateTrace("onFakeXpDrop");
             performAttack();
         }
     }
@@ -240,9 +243,10 @@ public class AttackTimerMetronomePlugin extends Plugin
             return;
         }
         combatExpEarned.get(event.getSkill()).addLast(event.getXp());
-        if (attackState == AttackState.DELAYED_FIRST_TICK)
+        if (inPreAttackWindow())
         {
             // We recompute attack speed here incase the hitsplat mattered (e.g. purging staff)
+            logStateTrace("onStatChanged");
             performAttack();
         }
     }
@@ -509,6 +513,7 @@ public class AttackTimerMetronomePlugin extends Plugin
                     // an instant attack. If its queued, don't trigger the cooldown yet.
                     if (isPlayerAttacking())
                     {
+                        logStateTrace("onInteractingChanged");
                         performAttack();
                     }
                     break;
@@ -541,6 +546,7 @@ public class AttackTimerMetronomePlugin extends Plugin
             case NOT_ATTACKING:
                 if (isAttacking)
                 {
+                    logStateTrace("onGameTick");
                     performAttack(); // Sets state to DELAYED_FIRST_TICK.
                 }
                 else
@@ -557,6 +563,7 @@ public class AttackTimerMetronomePlugin extends Plugin
                 { // Eligible for a new attack
                     if (isAttacking)
                     {
+                        logStateTrace("onGameTick");
                         performAttack();
                     }
                     else
@@ -614,6 +621,23 @@ public class AttackTimerMetronomePlugin extends Plugin
     @VisibleForTesting
     public void writeState(ByteArrayDataOutput outChannel)
     {
+        StringBuilder sb = getState();
+        byte[] bytes = sb.toString().getBytes(StandardCharsets.UTF_8);
+        outChannel.write(bytes);
+    }
+
+    public void logStateTrace(String trace)
+    {
+        if (!config.debugLogs())
+        {
+            return;
+        }
+        StringBuilder sb = getState();
+        log.debug("["+trace+"]: "+sb.toString());
+    }
+
+    private StringBuilder getState()
+    {
         StringBuilder sb = new StringBuilder();
         // @formatter:off
         sb.append("tickPeriod: "); sb.append(this.tickPeriod);sb.append(SEPARATOR);
@@ -626,9 +650,9 @@ public class AttackTimerMetronomePlugin extends Plugin
         sb.append("soundEffectTick: "); sb.append(this.soundEffectTick);sb.append(SEPARATOR);
         sb.append("soundEffectId: "); sb.append(this.soundEffectId);sb.append("\n");
         // @formatter:on
-        byte[] bytes = sb.toString().getBytes(StandardCharsets.UTF_8);
-        outChannel.write(bytes);
+        return sb;
     }
+
     private static final String SEPARATOR = ", ";
 
 
@@ -655,14 +679,27 @@ public class AttackTimerMetronomePlugin extends Plugin
 
         // This windowing safe guards of from late swaps inside a tick, if we have already rendered the tick
         // then we shouldn't perform another attack.
-        final boolean preAttackWindow = attackState == AttackState.DELAYED_FIRST_TICK && renderedState != attackState;
-        if (preAttackWindow && weaponMisMatch)
+        if (inPreAttackWindow() && weaponMisMatch)
         {
+            logStateTrace("checkForLateWeaponSwaps");
             // "Perform an attack" this is overwrites the last attack since we now know the user swapped
             // "Something" this tick, the equipped weapon detection will pick up specific weapon swaps. Even
             // swapping more than 1 weapon inside a single tick.
             performAttack();
         }
+    }
+
+    /**
+     * inPreAttackWindow returns true if and only if the plugin has computed an attack speed and
+     * determined we are attacking an NPC, but the timer has not been rendered yet. Hence there is time
+     * still to adjust the speed if new data would change the result.
+     *
+     * @return true if an attack is detected and the plugin has not yet rendered the timer for the
+     *         current attack, false in every other case.
+     */
+    private boolean inPreAttackWindow()
+    {
+        return attackState == AttackState.DELAYED_FIRST_TICK && renderedState != attackState;
     }
 
 }