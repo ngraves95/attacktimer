b66df0c3aace163487e0bf1d7fdf7e0d8591ee50
Lexer747 <lexer747@gmail.com>
2026-08-30 12:43:51 +0100

    version bump

--- src/main/java/com/attacktimer/AttackTimerMetronomeConfig.java
+++ src/main/java/com/attacktimer/AttackTimerMetronomeConfig.java
@@ -275,7 +275,7 @@ public interface AttackTimerMetronomeConfig extends Config
     @ConfigItem(
             position = 10000,
             keyName = "attacktimerVersion",
-            name = "Plugin version:            v1.2.13",
+            name = "Plugin version:            v1.3.0",
             description = ""
     )
     default void version()
